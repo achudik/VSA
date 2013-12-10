1b2be79bdf9503a74d5d5c95ead2f7f844bad285
achudik <achudik@gmail.com>
2013-12-10 22:25:06 +0100

    Update Ciselnik.java
    vymazal som importy

--- Ciselnik.java
+++ Ciselnik.java
@@ -1,12 +1,4 @@
-
-import net.useobjects.draw.drawable.GroupView;
-import net.useobjects.draw.drawable.TextView;
-
-//Digitalne zobrazenie
-public class Ciselnik {
-	private TextView text;
-	private String jednotka;
-
+tu sa mazalo
 	public Ciselnik(GroupView skupina, int positionX, int positionY, int hodnota, String jednotka) {
 		this.jednotka = jednotka;
 		text = new TextView(skupina, vytvorZobrazovanyText(hodnota), positionX, positionY);
